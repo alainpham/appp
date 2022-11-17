ffa488ec7f7b2d43dc994343a5c4876ea6e8b3aa
Alain Pham <alainphm@gmail.com>
2022-11-17 12:01:30 +0100

    Update install.sh

--- viber/install.sh
+++ viber/install.sh
@@ -3,4 +3,4 @@
 install-appimage \
     viber  \
     https://download.cdn.viber.com/desktop/Linux/viber.AppImage \
-    https://github.com/alainpham/aaap/raw/master/viber/icon.png
+    https://github.com/alainpham/appp/raw/master/viber/icon.png
