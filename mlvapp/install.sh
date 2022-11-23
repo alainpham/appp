cc28b87facf7b9de91c1c04e8ff7dd963e1b011a
apham <alainphm@gmail.com>
2022-11-24 00:09:12 +0100

    mlv

--- mlvapp/install.sh
+++ mlvapp/install.sh
@@ -2,5 +2,5 @@
 
 install-appimage \
     mlvapp \
-    https://github.com/ilia3101/MLV-App/releases/download/QTv1.13/MLV.App.v1.13.Linux.x86_64.AppImage \
+    https://github.com/ilia3101/MLV-App/releases/download/QTv1.14/MLV.App.v1.14.Linux.x86_64.AppImage \
     https://github.com/alainpham/appp/raw/master/mlvapp/icon.png
